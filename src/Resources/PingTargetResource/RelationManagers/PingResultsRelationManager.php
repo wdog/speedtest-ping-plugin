849ec535ea07bc6ba265eb80ba4be9e9b9b3984d
wdog <wdog666@gmail.com>
2024-03-26 17:31:56 +0100

    WIP

---
+++ src/Resources/PingTargetResource/RelationManagers/PingResultsRelationManager.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Wdog\Ping\Resources\PingTargetResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class PingResultsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'pingresults';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('ping'),
+                Forms\Components\TextInput::make('data')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('created_at')
+            ->columns([
+                Tables\Columns\TextColumn::make('ping')->suffix(' ms'),
+                Tables\Columns\TextColumn::make('data'),
+                Tables\Columns\TextColumn::make('created_at')->dateTime('Y-m-d H:i:s')->alignEnd(),
+            ])
+            ->defaultSort('created_at', 'desc')
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                // Tables\Actions\ViewAction::make(),
+                // Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
